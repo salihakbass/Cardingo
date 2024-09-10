79619468666f59b9e9454aa24d3e8b4db78f37ca
Salih Akbas <salihhakbass@gmail.com>
2024-09-10 16:10:48 +0300

    choosing level methods added

---
+++ app/src/main/java/com/example/cardingo/ui/fragment/chooselevel/ChooseLevelFragment.kt
@@ -0,0 +1,78 @@
+package com.example.cardingo.ui.fragment.chooselevel
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.cardingo.R
+import com.example.cardingo.databinding.FragmentChooseLevelBinding
+
+
+class ChooseLevelFragment : Fragment() {
+    private lateinit var binding : FragmentChooseLevelBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+
+        val sharedPreferences = requireContext().getSharedPreferences("SelectedLevel", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        binding.btnAFirst.setOnClickListener {
+            // Seçilen seviyeyi kaydet
+            editor.putString("level", "A1")
+            editor.apply()
+
+            val action = ChooseLevelFragmentDirections.actionChooseLevelFragmentToWordsFragment("A1")
+            findNavController().navigate(action)
+        }
+
+        binding.btnASecond.setOnClickListener {
+            editor.putString("level", "A2")
+            editor.apply()
+
+            val action = ChooseLevelFragmentDirections.actionChooseLevelFragmentToWordsFragment("A2")
+            findNavController().navigate(action)
+        }
+
+        binding.btnBFirst.setOnClickListener {
+            editor.putString("level", "B1")
+            editor.apply()
+
+            val action = ChooseLevelFragmentDirections.actionChooseLevelFragmentToWordsFragment("B1")
+            findNavController().navigate(action)
+        }
+
+        binding.btnBSecond.setOnClickListener {
+            editor.putString("level", "B2")
+            editor.apply()
+
+            val action = ChooseLevelFragmentDirections.actionChooseLevelFragmentToWordsFragment("B2")
+            findNavController().navigate(action)
+        }
+
+        binding.btnCFirst.setOnClickListener {
+            editor.putString("level", "C1")
+            editor.apply()
+
+            val action = ChooseLevelFragmentDirections.actionChooseLevelFragmentToWordsFragment("C1")
+            findNavController().navigate(action)
+        }
+
+        binding.btnCSecond.setOnClickListener {
+            editor.putString("level", "C2")
+            editor.apply()
+
+            val action = ChooseLevelFragmentDirections.actionChooseLevelFragmentToWordsFragment("C2")
+            findNavController().navigate(action)
+        }
+        return binding.root
+    }
+
+}
