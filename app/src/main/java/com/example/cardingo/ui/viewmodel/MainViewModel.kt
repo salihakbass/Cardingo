3a90748e752d058fd527d0792276050a867add55
Salih Akbas <salihhakbass@gmail.com>
2024-09-13 19:25:58 +0300

    viewmodel class deleted

--- app/src/main/java/com/example/cardingo/ui/viewmodel/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cardingo.ui.viewmodel
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.example.cardingo.data.entity.Words
-
-class MainViewModel : ViewModel() {
-    val shuffledData : MutableLiveData<List<Words>> = MutableLiveData()
-}
