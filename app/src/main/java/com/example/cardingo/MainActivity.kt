a7ebb352fc3aeba05a76a98a5b8f2f2e1ac341e9
Salih Akbas <salihhakbass@gmail.com>
2024-09-13 17:30:47 +0300

    install splash screen method added

--- app/src/main/java/com/example/cardingo/MainActivity.kt
+++ app/src/main/java/com/example/cardingo/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.cardingo
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment =
